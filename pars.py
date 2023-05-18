057efe105fc561131a09e2759a6c02b24c7b71ba
Casual-User <82218252+vlad-neep@users.noreply.github.com>
2023-05-18 16:48:35 +0300

    Update pars.py

--- pars.py
+++ pars.py
@@ -19,7 +19,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+browser = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Function to determine the matches that are already going, and then we call the function to find the page with the live broadcast of the match.
 def current_match():
